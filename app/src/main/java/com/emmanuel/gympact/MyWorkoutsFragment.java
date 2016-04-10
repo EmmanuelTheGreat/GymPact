fc4d2002a9b3be75dcfefdd3c4b7d3a7a0baf13d
user <emmanuel.l@hotmail.co.uk>
2016-04-10 09:56:43 +0100

    Login and register changes as of 10/04/16 09:56

--- app/src/main/java/com/emmanuel/gympact/MyWorkoutsFragment.java
+++ app/src/main/java/com/emmanuel/gympact/MyWorkoutsFragment.java
@@ -1,17 +1,19 @@
 package com.emmanuel.gympact;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by user on 07/04/2016.
  */
-public class MyWorkoutsFragment extends PreferenceFragment {
+public class MyWorkoutsFragment extends PreferenceFragment{
 
         View myView;
 
@@ -20,7 +22,31 @@ public class MyWorkoutsFragment extends PreferenceFragment {
 
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.my_workouts, container, false); // we are instantiating the myView object with the method inside of the inflater object with the first parameter of the inflator.inflate method being r.layout.my_workouts
-        return myView;
+
+            final Button chestButton =(Button)myView.findViewById(R.id.ChestBtn);
+            final Button backButton =(Button)myView.findViewById(R.id.BackBtn);
+            final Button legsButton =(Button)myView.findViewById(R.id.LegBtn);
+
+            chestButton.setOnClickListener(new View.OnClickListener(){
+            
+                @Override
+                public void onClick(View v) {
+                    Intent ChestIntent = new Intent(getActivity(), Chest.class);
+                    startActivity(ChestIntent);
+
+                }
+            });
+
+         /*   backButton.setOnClickListener(){
+
+            };
+
+            legsButton.setOnClickListener(){
+
+            };
+
+            */
+            return myView;
 
     }
     }
